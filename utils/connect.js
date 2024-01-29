7475eec73bf4b87a5f0686e1ff2fa4f962c8a479
mohan <mohankumar.kukkala @gmail.com>
2024-01-29 17:58:16 +0530

    env changes

--- utils/connect.js
+++ utils/connect.js
@@ -1,13 +1,16 @@
-const mongoose=require('mongoose')
-require('dotenv').config();
-async function connectDatabase(){
-    mongoose
-    .connect(process.env.MONGO_URL)
+const mongoose = require("mongoose");
+require("dotenv").config();
+async function connectDatabase() {
+  const mongoUri =
+    process.env.NODE_ENV === "PROD"
+      ? process.env.MONGO_URI_PROD
+      : process.env.NODE_ENV === "DEV"
+      ? process.env.MONGO_URI_DEV
+      : process.env.MONGO_URI_TEST;
+  mongoose
+    .connect(mongoUri)
     .then(() => console.log("MongoDb connected ..."))
-    .catch((err) => console.log(err,"Err at 07"));
+    .catch((err) => console.log(err, "Err at 07"));
 }
 console.log(process.env.MONGO_URL);
 module.exports = connectDatabase;
-
-
-
