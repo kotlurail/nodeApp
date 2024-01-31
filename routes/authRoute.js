7fd5b83aab4cd181e0745a682c9f7a510f3ccfe2
mohan <mohankumar.kukkala @gmail.com>
2024-01-31 12:49:32 +0530

    duplicateroute

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,21 @@
-const express = require('express');
-const passport = require('passport');
-const { createUser, loginUser, checkAuth, logout } = require('../controller/Auth');
+const express = require("express");
+const passport = require("passport");
+const {
+  createUser,
+  loginUser,
+  checkAuth,
+  logout,
+} = require("../controller/Auth");
 const router = express.Router();
 //  /auth is already added in base path
-router.post('/signup', createUser)
-.post('/login', passport.authenticate('local'), loginUser)
-.get('/check',passport.authenticate('jwt'), checkAuth)
-.get('/logout', logout)
+router
+  .post("/signup", createUser)
+  .post("/login", passport.authenticate("local"), loginUser)
+  .get("/check", passport.authenticate("jwt"), checkAuth)
+  .get("/logout", logout)
+  .get("/duplicate", (req, res) => {
+    res
+      .status(200)
+      .json({ name: "Mohan", Email: "mohankumar.kukkala@gmail.com" });
+  });
 module.exports = router;
