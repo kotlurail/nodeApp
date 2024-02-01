312e5fce9bc45ff38812a63e2f5bc3e3636d3493
mohan <mohankumar.kukkala @gmail.com>
2024-02-01 22:02:39 +0530

    commits

--- controller/User.js
+++ controller/User.js
@@ -6,6 +6,6 @@ exports.fetchUserDetails = async (req, res) => {
   // console.log(req.user,"requser at 06");
   const user = req.user;
   const userData = await User.findOne({ _id: user.id });
-  // console.log(userData,"userData at 8")
+  console.log("userData at 8");
   res.status(200).json({ name: userData.name, Email: userData.email });
 };
